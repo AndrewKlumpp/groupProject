6ca25a4b5f81f57ca7af8e383a96e3dc42f8f043
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-10-25 14:06:55 -0600

    added DisplayEvents component

--- client/src/views/Events.jsx
+++ client/src/views/Events.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
+import DisplayEvents from '../components/DisplayEvents';
 
 const Events = (props) => {
   return (
     <div>
       <Header title={'Events'} />
+      <DisplayEvents/>
     </div>
   )
 }
