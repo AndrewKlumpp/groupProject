9e50e79828d4cbc42dd60f6db273ea580275bbe1
Chris Horne <Christopher.Horne1986@gmail.com>
2024-10-29 13:18:14 -0400

    Updated login to get the user info.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,20 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './views/Home'
 import About from './views/About'
 import Events from './views/Events'
-import Login from './views/Login'
+//import Login from './views/Login'
+import LoginContent from './components/LoginContent'
 
 function App() {
+
+  const [user, setUser] = useState(null);
 
   return (
     <>
       <Routes>
         <Route index element={<Home />} />
-        <Route path='/events' element={<Events/>}/>
+        <Route path='/events' element={<Events user={user}/>}/>
         <Route path='/about' element={<About/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/login' element={<LoginContent setUser={setUser}/>}/>
       </Routes>
     </>
   )
