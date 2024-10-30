e3c5cbc5351bf795899ce8b69f1dc03856d021fc
Andrew Klumpp <AndrewOzcyKlumpp@gmail.com>
2024-10-30 17:00:03 -0600

    added sorting functionality for table on Events page

--- client/src/components/DisplayEvents.jsx
+++ client/src/components/DisplayEvents.jsx
@@ -4,65 +4,91 @@ import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 
 const DisplayEvents = (props) => {
-  const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
+
   useEffect(() => {
     axios.get('http://localhost:8000/api/events/get/events')
       .then((res) => {
-        console.log(res)
-        setEvents(res.data)
+        setEvents(res.data);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+        console.log(err);
+      });
+  }, []);
+
+  const sortEvents = (key) => {
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+
+    const sortedArray = [...events].sort((a, b) => {
+      if (key === 'eventDate') {
+        const dateA = new Date(a[key]);
+        const dateB = new Date(b[key]);
+        return direction === 'ascending' ? dateA - dateB : dateB - dateA;
+      } else {
+        return direction === 'ascending'
+          ? a[key].localeCompare(b[key])
+          : b[key].localeCompare(a[key]);
+      }
+    });
+
+    setEvents(sortedArray);
+    setSortConfig({ key, direction });
+  };
+
   return (
-    <div className='container'>
-      <table className='table table-striped table-dark border mt-3'>
+    <div className="container">
+      <table className="table table-striped table-dark border mt-3">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Location</th>
-            <th>Date</th>
+            <th onClick={() => sortEvents('eventName')} style={{ cursor: 'pointer' }}>
+              Name {sortConfig.key === 'eventName' ? (sortConfig.direction === 'ascending' ? '▲' : '▼') : ''}
+            </th>
+            <th onClick={() => sortEvents('eventLocation')} style={{ cursor: 'pointer' }}>
+              Location {sortConfig.key === 'eventLocation' ? (sortConfig.direction === 'ascending' ? '▲' : '▼') : ''}
+            </th>
+            <th onClick={() => sortEvents('eventDate')} style={{ cursor: 'pointer' }}>
+              Date {sortConfig.key === 'eventDate' ? (sortConfig.direction === 'ascending' ? '▲' : '▼') : ''}
+            </th>
             <th>Description</th>
             <th></th>
           </tr>
         </thead>
         <tbody>
-          {
-            events.map((event) => {
-              // Create a Date object from the event date string
-              const date = new Date(event.eventDate);
+          {events.map((event) => {
+            const date = new Date(event.eventDate);
+            const formattedDate = dayjs(date).format('MMMM D, YYYY');
 
-              // Format the date to "day/month/year"
-              const formattedDate = dayjs(date).format('MMMM D, YYYY');
-
-              return (
-                <tr key={event._id}>
-                  <td>{event.eventName}</td>
-                  <td>
-                    <a
-                      href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(event.eventLocation)}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {event.eventLocation}
-                    </a>
-                  </td>
-                  <td>{formattedDate}</td>
-                  <td>{event.eventDescription}</td>
-                  <td>
-                    <Link className="btn btn-success" to={`/update/event/${event._id}`}>
-                      Edit
-                    </Link>
-                  </td>
-                </tr>
-              );
-            })
-          }
+            return (
+              <tr key={event._id}>
+                <td>{event.eventName}</td>
+                <td>
+                  <a
+                    href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(event.eventLocation)}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {event.eventLocation}
+                  </a>
+                </td>
+                <td>{formattedDate}</td>
+                <td>{event.eventDescription}</td>
+                <td>
+                  <Link className="btn btn-success" to={`/update/event/${event._id}`}>
+                    Edit
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
-      <Link className='btn btn-success m-2' to={`/create/event`}>Add an Event</Link>
+      <Link className="btn btn-success m-2" to={`/create/event`}>Add an Event</Link>
     </div>
-  )
-}
+  );
+};
+
 export default DisplayEvents;
